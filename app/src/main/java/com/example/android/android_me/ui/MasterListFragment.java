65fe27284f41325b015565e34f10066d6a0ba9a4
Burhan Otour <botour-pc@Burhans-MacBook-Pro.local>
2019-11-17 20:51:45 +0100

    setup communication of a fragment to a main activity

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,11 +1,13 @@
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
@@ -15,12 +17,36 @@ public class MasterListFragment extends Fragment {
 
     public MasterListFragment() {}
 
+    OnImageClickListener mCallback;
+
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
+
+    // This is where a fragment attaches itself to a host activity
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnImageClickListener");
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fragmentView = inflater.inflate(R.layout.fragment_master_list, container, false);
-        GridView imagesGrid = (GridView) fragmentView.findViewById(R.id.image_grid);
-        imagesGrid.setAdapter(new MasterListAdapter(getContext(), AndroidImageAssets.getHeads()));
-        return fragmentView;
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.image_grid);
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        gridView.setAdapter(mAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallback.onImageSelected(position);
+            }
+        });
+        return rootView;
     }
 }
